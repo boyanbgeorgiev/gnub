8285ffb7451cacba1cc33e33ed2244b992aee08d
GeorgiMPastrakov <georgi.m.pastrakov2023@elsys-bg.org>
2024-03-15 20:29:04 +0200

    Updated the script

--- static/script.js
+++ static/script.js
@@ -1,76 +1,86 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var dropzone = document.getElementById('dropzone');
+document.addEventListener("DOMContentLoaded", function () {
+  var dropzone = document.getElementById("dropzone");
 
-    dropzone.addEventListener('drop', e => {
-        e.preventDefault();
-        dropzone.classList.remove('border-indigo-600');
-        var file = e.dataTransfer.files[0];
+  dropzone.addEventListener("drop", (e) => {
+    e.preventDefault();
+    dropzone.classList.remove("border-indigo-600");
+    var file = e.dataTransfer.files[0];
 
-        // Save the file to the input
-        var input = document.getElementById('file');
-        input.files = e.dataTransfer.files;
+    // Save the file to the input
+    var input = document.getElementById("file");
+    input.files = e.dataTransfer.files;
 
-        displayPreview(file);
-    });
+    displayPreview(file);
+  });
 
-    var input = document.getElementById('file');
+  var input = document.getElementById("file");
 
-    input.addEventListener('change', e => {
-        var file = e.target.files[0];
+  input.addEventListener("change", (e) => {
+    var file = e.target.files[0];
 
-        displayPreview(file);
-    });
+    displayPreview(file);
+  });
 
-    function displayPreview(file) {
-        if (file.type !== 'image/png' && file.type !== 'image/jpeg') {
-            alert('Only PNG and JPEG files are allowed.');
-            return;
-        }
-    
-        var reader = new FileReader();
-    
-        reader.onload = () => {
-            var preview = document.getElementById('preview');
-            preview.src = reader.result;
-            preview.classList.remove('hidden');
-    
-            // Display file name
-            var fileNameDisplay = document.getElementById('file-name');
-            fileNameDisplay.textContent = file.name;
-        };
-    
-        reader.readAsDataURL(file);
+  function displayPreview(file) {
+    if (file.type !== "image/png" && file.type !== "image/jpeg") {
+      alert("Only PNG and JPEG files are allowed.");
+      return;
     }
 
-    document.querySelector('#scan').addEventListener('submit', e => {
-        e.preventDefault();
+    var reader = new FileReader();
 
-        var fileInput = document.querySelector('input#file');
+    reader.onload = () => {
+      var preview = document.getElementById("preview");
+      preview.src = reader.result;
+      preview.classList.remove("hidden");
 
-        if (fileInput.files.length > 0) {
-            var file = fileInput.files[0];
+      // Display file name
+      var fileNameDisplay = document.getElementById("file-name");
+      fileNameDisplay.textContent = file.name;
+    };
 
-            var reader = new FileReader();
-            reader.onloadend = function() {
-                var base64 = reader.result;
+    reader.readAsDataURL(file);
+  }
 
-                fetch('/api/scan', {
-                    method: 'POST',
-                    body: JSON.stringify({ image: base64 }),
-                    headers: {
-                        'Content-Type': 'application/json'
-                    }
-                })
-                .then(response => response.json())
-                .then(data => {
-                    alert(data.message);
-                })
-                .catch(error => {
-                    console.error('Error:', error);
-                    alert('An error occurred while uploading the file.');
-                });
-            };
-            reader.readAsDataURL(file); // Converts the file to Base64
-        }
-    });
+  document.querySelector("#scan").addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    var fileInput = document.querySelector("input#file");
+
+    if (fileInput.files.length > 0) {
+      var file = fileInput.files[0];
+
+      var reader = new FileReader();
+      reader.onloadend = function () {
+        var base64 = reader.result;
+
+        fetch("/api/scan", {
+          method: "POST",
+          body: JSON.stringify({ image: base64 }),
+          headers: {
+            "Content-Type": "application/json",
+          },
+        })
+          .then((response) => response.json())
+          .then((data) => {
+            let name = data.message.name;
+            let description = data.message.description;
+
+            let nameElem = document.getElementById("shark-name");
+            let descElem = document.getElementById("shark-desc");
+            let resultsElem = document.getElementById("results");
+
+            resultsElem.classList.remove("hidden");
+
+            nameElem.textContent = name;
+            descElem.textContent = description;
+          })
+          .catch((error) => {
+            console.error("Error:", error);
+            alert("An error occurred while uploading the file.");
+          });
+      };
+      reader.readAsDataURL(file); // Converts the file to Base64
+    }
+  });
 });
