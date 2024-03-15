51bc534fd558af57301c956114426bb0aa383d50
NevenaDimitrovaa <157386055+NevenaDimitrovaa@users.noreply.github.com>
2024-03-15 17:22:52 +0200

    TrainAI
    training ai script, api folder

---
+++ api/trainai.py
@@ -0,0 +1,76 @@
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.applications import MobileNetV2
+from tensorflow.keras.layers import Dense, GlobalAveragePooling2D, Dropout
+from tensorflow.keras.models import Model
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
+import numpy as np
+
+train_dir = 'data/train'
+val_dir = 'data/validation'
+IMG_SIZE = (224, 224)  
+BATCH_SIZE = 32
+
+train_datagen = ImageDataGenerator(
+    rescale=1./255,
+    rotation_range=40,
+    width_shift_range=0.2,
+    height_shift_range=0.2,
+    shear_range=0.2,
+    zoom_range=0.2,
+    horizontal_flip=True,
+    fill_mode='nearest')
+
+val_datagen = ImageDataGenerator(rescale=1./255)
+
+train_generator = train_datagen.flow_from_directory(
+    train_dir,
+    target_size=IMG_SIZE,
+    batch_size=BATCH_SIZE,
+    class_mode='categorical')
+
+validation_generator = val_datagen.flow_from_directory(
+    val_dir,
+    target_size=IMG_SIZE,
+    batch_size=BATCH_SIZE,
+    class_mode='categorical')
+
+base_model = MobileNetV2(weights='imagenet', include_top=False, input_shape=(224, 224, 3))
+
+head_model = base_model.output
+head_model = GlobalAveragePooling2D()(head_model)
+head_model = Dense(1024, activation='relu')(head_model)
+head_model = Dropout(0.2)(head_model)  
+head_model = Dense(len(train_generator.class_indices), activation='softmax')(head_model)
+
+model = Model(inputs=base_model.input, outputs=head_model)
+
+for layer in base_model.layers:
+    layer.trainable = False
+
+model.compile(optimizer=Adam(learning_rate=0.0001), loss='categorical_crossentropy', metrics=['accuracy'])
+
+history_initial = model.fit(
+    train_generator,
+    steps_per_epoch=train_generator.n // BATCH_SIZE,
+    validation_data=validation_generator,
+    validation_steps=validation_generator.n // BATCH_SIZE,
+    epochs=10)
+
+for layer in base_model.layers[-20:]:  
+   layer.trainable = True
+
+model.compile(optimizer=Adam(learning_rate=0.00001),  
+              loss='categorical_crossentropy',
+              metrics=['accuracy'])
+
+model_checkpoint=tensorflow.keras.callbacks.ModelCheckpoint('checkpoint.ckpt', save_best_only=true, save_weights_only=True)
+history_fine = model.fit(
+    train_generator,
+    steps_per_epoch=train_generator.n // BATCH_SIZE,
+    validation_data=validation_generator,
+    validation_steps=validation_generator.n // BATCH_SIZE,
+    epochs=10,
+    callbacks=[model_checkpoint])
+model.save('shark_model.h5')
